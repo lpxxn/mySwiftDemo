1b00ad84499b1b4cacace0bfaa227a7759752c49
李鹏 <mi_duo@live.com>
2016-06-14 10:18:07 +0800

    facebookLoginView

---
+++ FaceBookLoginView/FaceBookLoginViewTests/FaceBookLoginViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  FaceBookLoginViewTests.swift
+//  FaceBookLoginViewTests
+//
+//  Created by peng.li on 16/6/14.
+//  Copyright © 2016年 pengli.xxn. All rights reserved.
+//
+
+import XCTest
+@testable import FaceBookLoginView
+
+class FaceBookLoginViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
